78dcc73c97d201952773e02cc8df321c0e8ad5d4
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-07-26 14:35:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// local module
+
+// const { add, a } = require("./local-1");
+// const { add: add2, a: a2 } = require("./local-2");
+
+// console.log(a2);
+
+// built in module
+
+const path = require("path");
+console.log(path.parse("/I:/learning-node/index.js"));
